04a52a2eacd9c5675d7bba5984c6c0804cddddc0
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-01-25 22:39:40 +0100

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import utilStyles from '../../styles/utils.module.css';
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <p>{postData.date}</p>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
+    );
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id);
+    return {
+      props: {
+        postData,
+      },
+    };
+}
